dadf7e0b5cf2cad4d89998c15678404a2261d148
何立伟 <heliwei@megvii.com>
2019-07-10 14:14:28 +0800

    solve 4

--- 4_find_median_sorted_arrays.cpp
+++ 4_find_median_sorted_arrays.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int find(vector<int> v,int b, int e, int target){
-    //cout<<"b: "<<b<<"e: "<<e<<endl;
-    //cout<<"mid: "<<v[(b+e)/2]<<endl;
     if(e - b == 1){
         return e;
     }
@@ -17,19 +15,25 @@ int find(vector<int> v,int b, int e, int target){
 
 int findIndexInArr(int a,vector<int> v){
     int size = v.size();
-    int index = find(v,-1,size,a);
+    int index = find(v, -1,size,a);
     return index;
 }
 
 int findRst(vector<int> v1, vector<int> v2, int b, int e, int target_index){
     int mid = (b + e)/2;
     int index_in_2 = findIndexInArr(v1[mid],v2);
-    if(e - b == 1){
-        return v2[target_index - mid];
-    }
     if(index_in_2 + mid == target_index){
         return v1[mid];
     }
+
+    if(e - b == 1){
+        if(b == -1){
+            return v2[target_index];
+        }else{
+            return v2[target_index - mid - 1];
+        }
+    }
+
     if(mid + index_in_2 > target_index){
         return findRst(v1,v2,b,mid,target_index);
     }else{
@@ -40,26 +44,41 @@ int findRst(vector<int> v1, vector<int> v2, int b, int e, int target_index){
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     int size1 = nums1.size();
     int size2 = nums2.size();
+    if(size1 == 0){
+        int idx = size2/2;
+        if(size2 %2 == 0){
+            double ret = ( (double)nums2[idx]+(double)nums2[idx-1] )/2;
+            return ret;
+        }else{
+            return nums2[idx];
+        }
+    }
+    if(size2 == 0){
+        int idx = size1/2;
+        if(size1%2 == 0){
+            double ret = ((double)nums1[idx]+(double)nums1[idx - 1])/2;
+            return ret;
+        }else{
+            return nums1[idx];
+        }
+    }
     int sum_size = size1 + size2;
     int index = sum_size /2;
-    int ret = 0;
-    cout<<"index: "<<index<<endl;
+    double ret = 0;
     if(sum_size %2 == 0){
         int r1 = findRst(nums1, nums2, -1, size1, index);
         int r2 = findRst(nums1, nums2, -1, size1, index - 1);
-        cout<<"r1: "<<r1<<" r2: "<<r2<<endl;
-        ret = (r1 + r2)/2;
+        ret = ((double)r1 + (double)r2)/2;
     }else{
         ret = findRst(nums1, nums2, -1, size1, index);
-        cout<<"ret: "<<ret<<endl;
     }
     return ret;
 }
 
 int main(){
-    vector<int> v{1,3,5,7,9};
-    vector<int> vv{2,4,6,8};
+    vector<int> v{3};
+    vector<int> vv{1,2,4};
     double d = findMedianSortedArrays(v,vv);
-    cout<<"rst: "<<d<<endl;
+    cout<<"d: "<<d<<endl;
     return 0;
 }
