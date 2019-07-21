8cec294116cd10a2a6bb234076c528aacc90af2c
He Liwei <heliwei@megvii.com>
2019-07-21 17:50:09 +0800

    finish problem 8

--- 8_my_atio.cpp
+++ 8_my_atio.cpp
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 int myAtoi(string str){
-    int INT_MAX = 0x7fffffff;
-    int INT_MIN = 0x80000000;
+    int MAX_INT = 0x7fffffff;
+    int MIN_INT = 0x80000000;
     int size = str.size();
     bool get_num = false;
     bool negative = false;
@@ -24,7 +24,7 @@ int myAtoi(string str){
                 return 0;
             }
         }else{
-            if(str[i] > '0' && str[i] < '9'){
+            if(str[i] >= '0' && str[i] <= '9'){
                 int num = str[i] - '0';
                 cout<<"num: "<<num<<endl;
                 rst = rst * 10 + num;
@@ -34,11 +34,15 @@ int myAtoi(string str){
             }
         }
     }
-    if(negative)
-    if(rst > INT_MAX){
-
-    }else if(rst < INT_MIN){
-
+    if(negative){
+        rst = -rst;
+    }
+    if(rst > MAX_INT){
+        return MAX_INT;
+    }else if(rst < MIN_INT){
+        return MIN_INT;
+    }else {
+        return (int)rst;
     }
 }
 
