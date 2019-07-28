0334b7face356ef4d1d8d117894cd45c89368e5c
He Liwei <heliwei@megvii.com>
2019-07-28 16:38:30 +0800

    modify 10

--- 10_is_match.cpp
+++ 10_is_match.cpp
@@ -18,25 +18,15 @@ int cntChar(string s, int idx, char c){
     return rst;
 }
 
-vector<int> findChar(string s, int idx, char c){
-    vector<int> v;
-    for(int i = idx; i < s.size(); i++){
-        if(s[i] == c){
-            v.push_back(i);
-        }
-    }
-    return v;
-}
-
-void dotStar(string s, int idx, char& c, int& dot_num, int& first_char_index){
+void dotStar(string s, int idx, int& dot_num, int& first_char_idx){
     // idx is the index after dot-star
     // c is the first char after dots and stars
     // dot_num is the count of dots
-    c = '-';
     dot_num = 0;
+    first_char_idx = -1;
     for(int i = idx; i < s.size(); i++){
         if(s[i] >= 'a' && s[i] <= 'z'){
-            c = s[i];
+            first_char_idx = i;
             return;
         }
         else if(s[i] == '.' && i + 1 < s.size() && s[i + 1] != '*'){
@@ -46,57 +36,41 @@ void dotStar(string s, int idx, char& c, int& dot_num, int& first_char_index){
 }
 
 bool isMatch(string s, string p){
+    if(s.size() == 0)
+        return true;
     int p_size = p.size();
     int si = 0;
     for(int pi = 0; pi < p_size; pi++){
         log("circle-------------------------\n");
-        log("pi: "+to_string(pi) + '\n');
-        log("si: "+to_string(si)+'\n');
+        log("current p: "+ p.substr(pi) + '\n');
+        log("current s: "+ s.substr(si)+'\n');
 
         if(p[pi] == '.'){
             if(pi + 1 < p_size && cntChar(p, pi+1, '*') > 0){
                 //dot with star
-                //FIXME: dot star does not mean a series of same char, it means any chars in random number.
-                /*log("dot with star\n");*/
-                //int star_num = cntChar(p, pi+1, '*');
-                //log("star_num: " + to_string(star_num));
-                //if(pi + star_num + 1 < p_size){
-                    //int char_num_p = cntChar(p, pi+star_num+1,s[si]);
-                    //int char_num_s = cntChar(s,si,s[si]);
-                    //if(char_num_s < char_num_p)
-                        //return false;
-                    //else{
-                        //pi += star_num + char_num_p;
-                        //si += char_num_s;
-                    //}
-                //}else{
-                    //int num_s = cntChar(s, si, s[si]);
-                    //if(si + num_s == s.size()){
-                        //return true;
-                    //}else{
-                        //return false;
-                    //}
-                /*}*/
+                log("dot with star\n");
                 int star_num = cntChar(p, pi + 1, '*');
                 int dot_num = 0;
-                char first_char = '-';
                 int first_char_idx = -1;
-                dotStar(s, pi + star_num, first_char, dot_num, first_char_idx);
-                if(first_char == '-'){
+                dotStar(p, pi + star_num + 1, dot_num, first_char_idx);
+                log("dot num: " + to_string(dot_num));
+                log("first_char_idx: " + to_string(first_char_idx));
+                if(first_char_idx == -1){
+                    log("no char after dot-star");
                     if(s.size() - si >= dot_num)
                         return true;
                     else
                         return false;
                 }else{
-                    vector<int> v_match_chars =  findChar(s, si + dot_num, first_char);
-                    if(v_match_chars.size() == 0) return false;
-                    else{
-                        for(int i = 0; i< v_match_chars.size(); i++){
-                            if(isMatch(s.substr(v_match_chars[i]), p.substr(first_char_idx)))
-                                return true;
+                    for(int i = si + dot_num; i <= s.size(); i++){
+                        log("recursive:================================================");
+                        log("s.substr(): " + s.substr(i));
+                        log("p.substr(): " + p.substr(first_char_idx));
+                        if(isMatch(s.substr(i), p.substr(first_char_idx))){
+                            return true;
                         }
-                        return false;
                     }
+                    return false;
                 }
             }else{
                 //dot without stars
@@ -106,21 +80,21 @@ bool isMatch(string s, string p){
         }else{
             if(pi + 1 < p_size && cntChar(p, pi+1, '*') > 0){
                 //char with star
-                log("dot with star\n");
+                log("char with star\n");
                 int star_num = cntChar(p, pi+1,'*');
-                if(pi+star_num+1 < p_size){
-                    int same_char_num = cntChar(p, pi+star_num+1,p[pi]);
-                    if(cntChar(s, si, p[pi]) < same_char_num)
-                        return false;
-                    else{
-                        pi += star_num+same_char_num;
-                        si += cntChar(s,si,p[pi]);
-                    }
+                int same_char_num_s = cntChar(s, si, p[pi]);
+                if(same_char_num_s == 0){
+                    pi += star_num;
                 }else{
-                    int num_s = cntChar(s,si,p[pi]);
-                    if(si + num_s == s.size())
-                        return true;
-                    else return false;
+                    for(int i = 0; i <= same_char_num_s; i++){
+                        log("recursive:================================================");
+                        log("round: " + to_string(i) + ".............");
+                        log("new p: " + p.substr(pi + star_num + 1));
+                        log("new s: " + s.substr(si + i));
+                        if(isMatch(s.substr(si + i), p.substr(pi + star_num + 1)))
+                            return true;
+                    }
+                    return false;
                 }
             }else{
                 //char without stars
@@ -141,8 +115,8 @@ bool isMatch(string s, string p){
 
 int main(){
     //test cnt chat
-    string s = "bcdddff";
-    string p = "a*bc.****.f*";
+    string s = "ab";
+    string p = ".*..c*";
     bool b = isMatch(s,p);
     log("result");
     log(to_string(b));
