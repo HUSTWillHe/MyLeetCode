6f1fab4987ddd770e2eddad2e6b6967916374bde
He Liwei <heliwei@megvii.com>
2019-08-02 15:14:13 +0800

    update script for creating new code file, and add sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <memory>
+#include <new>
+#include <complex>
+#include <exception>
+#include <functional>
+#include <utility>
+#include <typeinfo>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <climits>
+#include <cerrno>
+#include <ctime>
+#include <csignal>
+#include <csetjmp>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
+using namespace std;
+
+int main(int argc,char** argv){
+
+    return 0;
+}
