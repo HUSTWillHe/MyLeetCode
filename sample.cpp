0e31c2d0a71468751426a27e834b062964cbce67
HUSTWillHe <heliwei1994@gmail.com>
2019-08-05 00:51:17 +0800

    modify sample

--- sample.cpp
+++ sample.cpp
@@ -1,38 +1,7 @@
 #include <iostream>
-#include <iostream>
 #include <string>
-#include <bitset>
 #include <vector>
-#include <list>
-#include <deque>
-#include <queue>
-#include <set>
-#include <map>
 #include <algorithm>
-#include <memory>
-#include <new>
-#include <complex>
-#include <exception>
-#include <functional>
-#include <utility>
-#include <typeinfo>
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstdint>
-#include <cstring>
-#include <climits>
-#include <cerrno>
-#include <ctime>
-#include <csignal>
-#include <csetjmp>
-
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/time.h>
 
 using namespace std;
 
