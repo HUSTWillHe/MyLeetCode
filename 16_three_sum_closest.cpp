a6737a9e2b0ecc1867604e08fea38e568f37cb71
He Liwei <heliwei@megvii.com>
2019-08-11 16:55:24 +0800

    finish problem 16

--- 16_three_sum_closest.cpp
+++ 16_three_sum_closest.cpp
@@ -11,39 +11,32 @@ int threeSumClosest(vector<int> & nums, int target){
     int min_delta = -1;
     int rst_sum;
     for(int i = 0; i < size; i++){
-        cout<<"circle index: "<<i<<"===================================== "<<endl;
         for(int b = i + 1, e = size -1; b < e; ){
-            cout<<"begin index: "<<b<<" end index: "<<e<<endl;
             if(nums[i] + nums[b] + nums[e] > target){
                 int delta = nums[i] + nums[b] + nums[e] - target;
                 if(min_delta == -1){
                     min_delta = delta;
-                    rst_sum = target + delta;
+                    rst_sum = nums[i] + nums[b] + nums[e];
                 }else{
                     if(delta < min_delta){
                         min_delta = delta;
                         rst_sum = target + delta;
-                        cout<<"find smaller large: "<<"begin idx: "<<b<<" end idx: "<<e<<endl;
-                        cout<<"large is: "<<rst_sum<<endl;
                     }
                 }
                 e--;
             }else if(nums[i] + nums[b] + nums[e] < target){
-                int delta = target - nums[i] + nums[b] + nums[e];
+                int delta = target - nums[i] - nums[b] - nums[e];
                 if(min_delta == -1){
                     min_delta = delta;
-                    rst_sum = target - delta;
+                    rst_sum = nums[i] + nums[b] + nums[e];
                 }else{
                     if(delta < min_delta){
                         min_delta = delta;
                         rst_sum = target - delta;
-                        cout<<"find smaller less: "<<"begin idx: "<<b<<" end idx: "<<e<<endl;
-                        cout<<"less is: "<<rst_sum<<endl;
                     }
                 }
                 b++;
             }else{
-                cout<<"find smaller equal: "<<"begin idx: "<<b<<" end idx: "<<e<<endl;
                 return target;
             }
         }
