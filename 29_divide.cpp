ad9010df3445eb86632441c8f727e0f0ed93f29c
He Liwei <heliwei@megvii.com>
2019-09-07 02:20:54 +0800

    finish 29

--- 29_divide.cpp
+++ 29_divide.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+int MIN_INT = 0x80000000;
+int MAX_INT = 0x7FFFFFFF;
+
 int sub_div(int dividend, int divisor){
     if(dividend < divisor) return 0;
     int i = 0;
@@ -18,38 +21,36 @@ int sub_div(int dividend, int divisor){
 }
 
 int divide(int dividend, int divisor){
-    int MIN_INT = 0x80000000;
-    int MAX_INT = 0x7FFFFFFF;
     if(dividend > MAX_INT || divisor > MAX_INT || dividend < MIN_INT || divisor < MIN_INT){
         return MAX_INT;
     }
-    if(dividend < 0 && divisor < 0){
-        if(dividend == MIN_INT){
-            if(divisor == -1)
-                return MAX_INT;
-            else if(divisor == MIN_INT){
-                return 1;
-            }
-            else{
-                return sub_div(0x40000000, -divisor) + sub_div(0x40000000, -divisor);
-            }
+    if(dividend == MIN_INT){
+        if(divisor == 1) return MIN_INT;
+        if(divisor == -1) return MAX_INT;
+        if(divisor == MIN_INT) return 1;
+        if(divisor < 0){
+            int a = MIN_INT - divisor;
+            return sub_div(-a, -divisor) + 1;
+        }else{
+            int a = MIN_INT + divisor;
+            return -sub_div(-a, divisor) - 1;
         }
-        else if(divisor == MIN_INT){
-            return 0;
-        }else
+    }else if(divisor == MIN_INT){
+        return 0;
+    }
+    if( dividend < 0){
+        if(divisor < 0){
             return sub_div(-dividend, -divisor);
-    }
-    else if(dividend < 0){
-        if(dividend == MIN_INT){
-            if(divisor == 1) return MIN_INT;
-            return -sub_div(0x40000000, divisor) - sub_div(0x40000000, divisor);
+        }else{
+            return -sub_div(-dividend, divisor);
         }
-        return -sub_div(-dividend, divisor);
-    }else if(divisor < 0){
-        if(divisor == MIN_INT) return 0;
-        return -sub_div(dividend, -divisor);
+    }else{
+        if(divisor < 0){
+            return -sub_div(dividend, -divisor);
+        }else{
+            return sub_div(dividend, divisor);
+        }
     }
-    else return sub_div(dividend, divisor);
 }
 
 int main(int argc,char** argv){
