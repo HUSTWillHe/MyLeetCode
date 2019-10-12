2f3640c975fc4a70844e312525c8c769708a76d5
He Liwei <heliwei@megvii.com>
2019-10-12 22:42:57 +0800

    modify 44

--- 44_is_match.py
+++ 44_is_match.py
@@ -4,32 +4,47 @@
 from typing import List
 
 class Solution:
-    def allStar(self, s) ->bool:
+    def allStar(self, s) -> bool:
         for i in s:
             if i != "*":
                 return False
         return True
 
-    def simpleFind(self, s:str, sub:str) -> List[int]:
-        ans = []
-        last = s
-        while len(last) < len(sub):
-            idx = last.find(sub)
-            if idx == -1:
-                return ans
+    def countQuestion(self, s:str) -> List[int]:
+        size = len(s)
+        if size == 0:
+            return [0, 0]
+        head = tail = 0
+        for i in range(size):
+            if s[i] == '?':
+                head += 1
             else:
-                ans.append(idx)
-                last = s[idx+1:]
+                break;
+        for i in range(size - 1, -1, -1):
+            if s[i] == '?':
+                tail += 1
+            else:
+                break
+        return [head, tail]
+
+    def indexAllQuesion(self, s:str)->List[int]:
+        ans = []
+        for i in range(len(s)):
+            if s[i] == "?":
+                ans.append(i)
         return ans
 
-    def findIdx(self, s:str, sub:str) -> List[int]:
-        ans = []
-        if s.find("?") == -1:
-            return self.simpleFind(s,sub)
-        while s.find("?") != -1:
+    def nearestIdx(self, s:str, sub:str) -> int:
+        head, tail = self.countQuestion(sub)
+        new_s = s[head:-tail]
+        idx_q = self.indexAllQuesion(new_s)
 
+        return 0
 
     def isMatch(self, s, p):
+        '''
+        process star singly, process ? and normal characters in one method.
+        '''
         if p == "" and s == "":
             return True
         if p == "" and s != "":
@@ -43,6 +58,10 @@ class Solution:
 
 def main():
     s = Solution()
+    a = "hqpgh???hgap8hg??"
+    l = s.indexAllQuesion(a)
+    for i in l:
+        print(i)
     return
 
 if __name__ == "__main__":
