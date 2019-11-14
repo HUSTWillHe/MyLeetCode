d6bd0e6c837ef237cc639ee872d7c4bff0b92276
He Liwei <heliwei@megvii.com>
2019-11-14 16:04:50 +0800

    finish 192:

---
+++ 192_word_frequency.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat 192_word_frequency.txt | xargs -n 1 | awk '{
+if($1 in data) 
+	data[$1] += 1
+else 
+	data[$1] = 1
+}
+END {
+	for (str in data)
+		print data[str], str
+}' | sort -rn | awk '{print $2, $1}'
