0540ace1eff03b327ff02483369dee1c1a9cbb72
He Liwei <heliwei@megvii.com>
2019-11-15 15:59:01 +0800

    finish 51, but time over

--- 51_solve_n_queens.cpp
+++ 51_solve_n_queens.cpp
@@ -2,69 +2,84 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <set>
 
 using namespace std;
 
 class Solution {
 public:
-	vector<vector<string>> allDots(int n){
-		vector<string> vs(n, '.');
-		vecotr<vector<string>> vvs(n, vs);
-		return vvs;
+	string int2str(int i, int length){
+		string s(length, '.');
+		s[i] = 'Q';
+		return s;
 	}
 
-	vector<vector<string>> convertStringFormating(vector<vector<pair<int, int>>> int_result, int n){
-		vector<vector<string>> string_result = allDots(n);
+	vector<vector<string>> convertStringFormating(vector<vector<int>> int_result, int n){
+		vector<vector<string>> string_result;
+		vector<string> sub_result;
 		for(auto vi: int_result){
+			sub_result.clear();
 			for(auto i: vi){
-				string s = int2Str(i, n);
-				sub_result.push_back(s);
+				sub_result.push_back(int2str(i, n));
 			}
 			string_result.push_back(sub_result);
 		}
 		return string_result;
 	}
 	
-	bool isFirstInCur(int i, vector<pair<int, int>> cur){
-		for(auto a: cur){
-			if(i == a.first)
-				return true;
-		}
-		return false;
-	}
-
-	bool isAttack(int i, int k, vector<pair<int, int>> cur){
-		for(auto a: cur){
-			if(k == cur.second || k + i == cur.first + cur.second ||
-					i - k == cur.first - cur.second)
-				return true;
-		}
-		return false;
-	}
-
-	void recursive(vector<pair<int, int>>& cur, vector<vector<pair<int, int>>>& result, int n){
+	void recursive(vector<int>& cur, vector<vector<int>>& result, int n){
 		if(cur.size() == n)
 			result.push_back(cur);
 		for(int i = 0; i < n; i++){
-			if(isFirstInCur(i, cur))
-				continue;
-			for(int k  = 0; k < n; k++){
-				if(isAttack(i, k, cur))
-					continue;
-				else{
-					cur.push_back(pair(i, k));
-					recursive(cur, result, n);
-					cur.pop_back();
-				}
+			if(find(cur.begin(), cur.end(), i) == cur.end()){
+				cur.push_back(i);
+				recursive(cur, result, n);
+				cur.pop_back();
+			}
+		}
+	}
+
+	bool isAttack(vector<int> one_result){
+		int col = 0;
+		set<int> sum;
+		set<int> delta;
+		for(auto i: one_result){
+			if(find(sum.begin(), sum.end(), i + col) == sum.end()){
+				sum.insert(i + col);
+			}else{
+				return true;
+			}
+			if(find(delta.begin(), delta.end(), i - col) == delta.end()){
+				delta.insert(i - col);
+			}else{
+				return true;
+			}
+			col ++;
+		}
+		return false;
+	}
+
+	/**
+	 * @function: 检查对角线攻击
+	 * @param[in][out]: int_result
+	 * @return void 
+	 **/
+	void recheckResult(vector<vector<int>>& int_result){
+		for(int i = int_result.size() - 1; i >= 0; i--){
+			vector<vector<int>>::iterator it = int_result.begin() + i;
+			if(isAttack(*it)){
+				int_result.erase(it);
 			}
 		}
 	}
 
 	vector<vector<string>> solveNQueens(int n){
-		vector<vector<pair<int, int>>> int_result;
+		vector<vector<int>> int_result;
 		vector<vector<string>> string_result;
-		vector<pair<int, int>> cur;
+		vector<int> cur;
 		recursive(cur, int_result, n);
+		recheckResult(int_result);
 		string_result = convertStringFormating(int_result, n);
 		return string_result;
 	}
@@ -82,7 +97,7 @@ void displayQueen(vector<vector<string>> queen){
 
 int main(int argc,char** argv){
 	Solution s;
-	vector<vector<string>> result = s.solveNQueens(3);
+	vector<vector<string>> result = s.solveNQueens(4);
 	displayQueen(result);
     return 0;
 }
