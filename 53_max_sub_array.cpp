a8f1e0683fbdb2d559acb1194074ade9dff72d09
He Liwei <heliwei@megvii.com>
2019-11-18 16:57:23 +0800

    time over version

--- 53_max_sub_array.cpp
+++ 53_max_sub_array.cpp
@@ -8,22 +8,12 @@ using namespace std;
 class Solution {
 public:
 	vector<int> findLeftBoundary(const vector<int>& nums){
-		int cur_sum = 0;
 		vector<int> leftBoundary;
 		for(int i = 0; i < nums.size(); i++){
-			if((i-1 == -1 || nums[i-1] < 0) && nums[i] > 0){
-				if(cur_sum <= 0){
-					if(!leftBoundary.empty()){
-						leftBoundary.pop_back();
-					}
-				}
+			if((i-1 == -1 || nums[i-1] <= 0) && nums[i] > 0){
 				leftBoundary.push_back(i);
-				cur_sum = 0;
 			}
-			cur_sum += nums[i];
 		}
-		if(cur_sum <= 0)
-			leftBoundary.pop_back();
 		return leftBoundary;
 	}
 
@@ -31,8 +21,8 @@ public:
 		reverse(nums.begin(), nums.end());
 		vector<int> reverse_boundary = findLeftBoundary(nums);
 		int size = nums.size();
-		for(int i = 0; i < size; i++){
-			nums[i] = size - 1 - nums[i];
+		for(int i = 0; i < reverse_boundary.size(); i++){
+			reverse_boundary[i] = size - 1 - reverse_boundary[i];
 		}
 		return reverse_boundary;
 	}
@@ -73,7 +63,7 @@ public:
 	
 	int range_sum(const vector<int>& nums, int left, int right){
 		int sum = 0;
-		for(int i = left; i < right; i++){
+		for(int i = left; i <= right; i++){
 			sum += nums[i];
 		}
 		return sum;
@@ -84,16 +74,6 @@ public:
 			return 0;
 		vector<int> leftBoundary = findLeftBoundary(nums);
 		vector<int> rightBoundary = findRightBoundary(nums);
-		cout<<"left boundary"<<endl;
-		for(auto a: leftBoundary){
-			cout<<a<<'\t';
-		}
-		cout<<endl;
-		cout<<"right boundary"<<endl;
-		for(auto a: rightBoundary){
-			cout<<a<<'\t';
-		}
-		cout<<endl;
 		if(leftBoundary.size() == 0 || rightBoundary.size() == 0){
 			return findMaxIndividual(nums);
 		}else{
@@ -103,13 +83,11 @@ public:
 					if(r >= l){
 						int cur_sum = range_sum(nums, l, r);
 						if(cur_sum > max_sum){
-							cout<<"new hight: "<<cur_sum<<" left: "<<l<<" right: "<<r<<endl;
 							max_sum = cur_sum;
 						};
 					}
 				}
 			}
-			cout<<"the anwser is: "<<max_sum<<endl;
 			return max_sum;
 		}
 	}
@@ -121,20 +99,32 @@ void test_find_max(vector<int>& nums){
 	cout<<"the max is: "<<result<<endl;
 }
 
+void test_findLeftBoundary(const vector<int>& nums){
+	Solution s;
+	vector<int> left = s.findLeftBoundary(nums);
+	cout<<"left boundary"<<endl;
+	for(auto a: left){
+		cout<<a<<'\t';
+	}
+	cout<<endl;
+}
+
+void test_findRightBoundary(const vector<int>& nums){
+	Solution s;
+	vector<int> rightBoundary = s.findRightBoundary(nums);
+	cout<<"right boundary: "<<endl;
+	for(auto a: rightBoundary){
+		cout<<a<<'\t';
+	}
+	cout<<endl;
+}
+
 int main(int argc,char** argv){
 	Solution s;
-	vector<int> v;
-	v.push_back(3);
-	v.push_back(-5);
-	v.push_back(9);
-	v.push_back(-2);
-	v.push_back(2);
-	v.push_back(-7);
-	v.push_back(8);
-	v.push_back(-1);
-	v.push_back(2);
-	v.push_back(-3);
+	vector<int> v{0, 2, 0, 2};
 	//test_find_max(v);
+	test_findRightBoundary(v);
+	test_findLeftBoundary(v);
 	s.maxSubArray(v);
     return 0;
 }
