cfab38c64af213d8c25097eb7c6b986df8bd7263
He Liwei <heliwei@megvii.com>
2019-11-21 00:58:13 +0800

    update newlt

---
+++ solutions/59_generate_matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	
+};
+
+int main(int argc,char** argv){
+	Solution s;
+	s.
+    return 0;
+}
