d1fefe3c9596e15b4ddf98cc9f89f659db6432e0
He Liwei <heliwei@megvii.com>
2019-11-21 21:36:07 +0800

    backup local work

--- solutions/59_generate_matrix.cpp
+++ solutions/59_generate_matrix.cpp
@@ -6,10 +6,40 @@
 using namespace std;
 
 class Solution {
+private:
+	vector<vector<int>> result;
+	int size;
 public:
+	enum Boundary{
+		up=0,
+		right,
+		bottom,
+		left
+	};
+
+	struct Position{
+		int round;
+		Boundary boundary;
+	};
+
+	void initMatrix(){
+		vector<int> v(size, 0);
+		vector<vector<int>> matrix(size, v);
+		result = matrix;
+	}
+
+	Position getPosition(int col, int row){
+
+	}
+
 	vector<vector<int>> generateMatric(int n) {
-		vector<int> v(n, 0);
-		vector<vector<int>> result(n, v);
+		size = n;
+		initMatrix();
+		for(int i = 0; i < n; i++) {
+			for(int k = 0; k < n; k++){
+
+			}
+		}
 		for(auto a: result){
 			for(auto b: a){
 				cout<<b<<'\t';
