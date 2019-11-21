c9c24bc7432a9f96f6a78f101b14ee499c916390
He Liwei <heliwei1994@gmail.com>
2019-11-22 01:27:59 +0800

    add solution to problem 59

--- solutions/59_generate_matrix.cpp
+++ solutions/59_generate_matrix.cpp
@@ -9,49 +9,60 @@ class Solution {
 private:
 	vector<vector<int>> result;
 	int size;
+	int current = 1;
+
 public:
-	enum Boundary{
-		up=0,
-		right,
-		bottom,
-		left
-	};
-
-	struct Position{
-		int round;
-		Boundary boundary;
-	};
-
 	void initMatrix(){
 		vector<int> v(size, 0);
 		vector<vector<int>> matrix(size, v);
 		result = matrix;
 	}
 
-	Position getPosition(int col, int row){
-
+	void oneRound(int n){
+		result[n][n] = current;
+		int col = n;
+		int row = n;
+		for(int i = 0; i < size - 1 - n * 2; i++){
+			result[col][++row] = ++current;
+		}
+		for(int i = 0; i < size - 1 - n * 2; i++){
+			result[++col][row] = ++current;
+		}
+		for(int i = 0; i < size - 1 - n * 2; i++){
+			result[col][--row] = ++current;
+		}
+		for(int i = 0; i < size - 2 - n * 2; i++){
+			result[--col][row] = ++current;
+		}
+		current ++;
+		//cout<<"-------------------------"<<endl;
+		//printResult();
 	}
 
-	vector<vector<int>> generateMatric(int n) {
-		size = n;
-		initMatrix();
-		for(int i = 0; i < n; i++) {
-			for(int k = 0; k < n; k++){
-
-			}
-		}
+	void printResult(){
 		for(auto a: result){
 			for(auto b: a){
 				cout<<b<<'\t';
 			}
 			cout<<endl;
 		}
+	}
+
+	vector<vector<int>> generateMatrix(int n) {
+		size = n;
+		initMatrix();
+
+		for(int i = 0; i < (size + 1)/2; i++){
+			oneRound(i);
+		}
+
+		//printResult();
 		return result;
 	}
 };
 
 int main(int argc,char** argv){
 	Solution s;
-	s.generateMatric(3);
+	s.generateMatrix(5);
     return 0;
 }
