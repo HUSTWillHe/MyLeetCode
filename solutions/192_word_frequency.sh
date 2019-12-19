862cc0b750209cf89b454b654eeaf0d358daee09
He Liwei <heliwei@megvii.com>
2019-12-19 17:38:43 +0800

    solve problem 192

--- solutions/192_word_frequency.sh
+++ solutions/192_word_frequency.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cat 192_word_frequency.txt | xargs -n 1 | awk '{
-if($1 in data) 
-	data[$1] += 1
-else 
-	data[$1] = 1
-}
-END {
-	for (str in data)
-		print data[str], str
-}' | sort -rn | awk '{print $2, $1}'
+#cat 192_word_frequency.txt | xargs -n 1 | awk '{
+#if($1 in data) 
+#	data[$1] += 1
+#else 
+#	data[$1] = 1
+#}
+#END {
+#	for (str in data)
+#		print data[str], str
+#}' | sort -rn | awk '{print $2, $1}'
+
+cat words.txt | xargs -n 1 | sort | uniq -c | sort -k1r | awk '{print $2,$1}'
